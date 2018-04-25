1e1d6d0428336b553df146ef8dee96835aea25b2
LeonardoGaldino <lcgm@cin.ufpe.br>
2018-04-24 22:50:24 -0300

    Ends solutions to third list

--- list3.hs
+++ list3.hs
@@ -49,7 +49,7 @@ sum100FirstsSquares :: Integer
 sum100FirstsSquares = sum [x*x | x <- [1..100]]
 
 -- Returns all pairs (x,y) for x in [0..m] and y in [0..y] 
-grid :: Int -> Int -> [(Int, Int)]
+grid :: (Integral a) => a -> a -> [(a, a)]
 grid m n = [(x,y) | x <- [0..m], y <- [0..n]]
 
 -- Merges two ordered lists
@@ -60,3 +60,90 @@ merge [] ys = ys
 merge (x:xs) (y:ys)
 	| x < y = (x:(merge xs (y:ys)))
 	| otherwise = (y:(merge (x:xs) ys))
+
+-- Splits list into two (in the half)
+halves :: (Ord a) => [a] -> ([a], [a])
+halves xs = (take firstHalf xs, reverse (take secondHalf (reverse xs)))
+	where
+		firstHalf
+			| odd $ len = (len `div` 2 ) + 1
+			| otherwise = div len 2
+			where
+				len = length xs
+		secondHalf = len - firstHalf
+			where
+				len = length xs
+
+-- MergeSort implementation 
+msort :: (Ord a) => [a] -> [a]
+msort [] = []
+msort [b] = [b]
+msort xs = merge (msort left) (msort right)
+	where
+		left = fst $ halves xs
+		right  = snd $ halves xs
+
+-- Receives list of functions and list of values and apply each value to each function
+applyUnaryFunctions :: [(a -> b)] -> [a] -> [b]
+applyUnaryFunctions [] [] = []
+applyUnaryFunctions [] vs = []
+applyUnaryFunctions fs [] = []
+applyUnaryFunctions (f:fs) (v:vs) = (f v):(applyUnaryFunctions fs vs) 
+
+-- Receives list of functions and list of values and apply each value to each function and returns unary function
+applyBiFunctions :: [(a -> b -> c)] -> [a] -> [(b -> c)]
+applyBiFunctions [] [] = []
+applyBiFunctions [] vs = []
+applyBiFunctions fs [] = []
+applyBiFunctions (f:fs) (v:vs) = (f v):(applyBiFunctions fs vs) 
+
+data WeekDay = Sunday | Monday | Tuesday | Wednesday | Thursday | Friday | Saturday
+
+instance Show WeekDay where
+	show (Sunday) = "Sunday"
+	show (Monday) = "Monday"
+	show (Tuesday) = "Tuesday"
+	show (Wednesday) = "Wednesday"
+	show (Thursday) = "Thursday"
+	show (Friday) = "Friday"
+	show (Saturday) = "Saturday"
+
+instance Enum WeekDay where
+	fromEnum Sunday = 0
+	fromEnum Monday = 1
+	fromEnum Tuesday = 2
+	fromEnum Wednesday = 3
+	fromEnum Thursday = 4
+	fromEnum Friday = 5
+	fromEnum Saturday = 6
+
+	toEnum 0 = Sunday
+	toEnum 1 = Monday
+	toEnum 2 = Tuesday
+	toEnum 3 = Wednesday
+	toEnum 4 = Thursday
+	toEnum 5 = Friday
+	toEnum 6 = Saturday
+
+instance Ord WeekDay where
+	a <= b = fromEnum a < fromEnum b
+
+-- This way, we don't have to write every pair of week day
+instance Eq WeekDay where
+	(a) == (b) = fromEnum a == fromEnum b
+
+-- Sort weekdays
+sortWeekDays :: [WeekDay] -> [WeekDay]
+sortWeekDays [] = []
+sortWeekDays xs = msort xs
+
+-- (Weekday, date) Weekday and returns all dates that are the same weekday as the second paremeter
+sameWeekDay :: [(WeekDay, Int)] -> WeekDay -> [Int]
+sameWeekDay [] _ = []
+sameWeekDay tps wkd = [snd tp | tp <- tps, fst tp == wkd]
+
+-- Receives the first WeekDay of the month and returns all the days
+printMonth :: WeekDay -> [(WeekDay, Int)]
+printMonth firstDay = [((toEnum (mod (offset+day-1) 7)::WeekDay), day) | day <- [1..31]]
+	where
+		offset = fromEnum firstDay
